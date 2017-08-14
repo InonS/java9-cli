e95d5c10b36da13f5944677a43d92339183a9bab
Inon Sharony <InonShar@TAU.ac.IL>
2017-08-14 11:10:50 +0300

    Initial commit

---
+++ src/ml/sharony/java9cli/Main.java
@@ -0,0 +1,9 @@
+package ml.sharony.java9cli;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // IntStream.range(0, 100).forEach(i -> System.out.print("מאור שרוני "));
+    }
+}
